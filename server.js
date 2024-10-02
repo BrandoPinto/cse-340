351dbba0a18e1b201ea49a2dadc96bf17165db81
unknown <bpintochecya@gmail.com>
2024-10-02 07:08:52 -0500

    cse-1

--- server.js
+++ server.js
@@ -5,35 +5,40 @@
 /* ***********************
  * Require Statements
  *************************/
-const express = require("express")
-const expressLayouts = require("express-ejs-layouts")
-const env = require("dotenv").config()
-const app = express()
+const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
+const env = require("dotenv").config();
+const app = express();
 
 /* ***********************
-* View Engine and Template
-**************************/
-app.set("view engine", "ejs")
-app.use(expressLayouts)
-app.set("layout", "./layouts/layout") // not at views root
+ * View Engine and Template
+ ************************/
+app.set("view engine", "ejs");
+app.use(expressLayouts);
+app.set("layout", "./layouts/layout"); // Layout not at views root
+
+/* ***********************
+ * Serve Static Files (CSS, Images, etc.)
+ ************************/
+app.use(express.static('public')); // Servir archivos estáticos desde 'public'
 
 /* ***********************
  * Routes
  *************************/
 app.get('/', (req, res) => {
-  res.render('index', { title: "Home" }) // Asegúrate de que 'views/index.ejs' existe
-})
+  res.render('index', { title: "Home" }); // Asegúrate de que 'views/index.ejs' existe
+});
 
 /* ***********************
  * Local Server Information
  * Values from .env (environment) file
  *************************/
-const port = process.env.PORT 
-const host = process.env.HOST
+const port = process.env.PORT
+const host = process.env.HOST 
 
 /* ***********************
  * Log statement to confirm server operation
  *************************/
 app.listen(port, () => {
-  console.log(`app listening on ${host}:${port}`)
-})
+  console.log(`App listening on ${host}:${port}`);
+});
