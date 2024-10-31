7bb5a87878cc098c93866b64a921d0e1be627cde
BrandoPinto <bpintochecya@gmail.com>
2024-10-31 17:57:58 -0500

    w3

---
+++ models/inventory-model.js
@@ -0,0 +1,14 @@
+const pool = require("../database/index");
+
+/**************************
+ * Get all Classification data
+ **************************/
+async function getClassifications() {
+  let statement =
+    "SELECT * FROM public.classification ORDER BY classification_name";
+  return await pool.query(statement);
+}
+
+module.exports = {
+    getClassifications,
+  };
